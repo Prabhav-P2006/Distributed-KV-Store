89943333d6f8fbdb2e6bb3a8de6922e530fe2301
Prabhav-P2006 <withprabhav@gmail.com>
2026-04-16 23:08:45 +0530

    Cleanup

--- shaunStore/server/tmp-cluster-config/client-15.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo 'Client 5: validator waiting for replication'
-sleep 5
-'/Users/prabhavp/dbms project/shaunStore/server/scripts/resp_client.sh' 127.0.0.1 8010 GET bulk:1
-'/Users/prabhavp/dbms project/shaunStore/server/scripts/resp_client.sh' 127.0.0.1 8010 GET vip:1
